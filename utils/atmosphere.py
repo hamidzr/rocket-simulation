8055835caf4b9932e76261e08293a81fbf707e08
Hamid Zare <dellydela@gmail.com>
2018-04-19 17:19:56 -0500

    moved filter and isreal to helpers

--- utils/atmosphere.py
+++ utils/atmosphere.py
@@ -1,20 +1,5 @@
 import numpy as np
-from utils.helpers import logger
-
-def filter(pred, lst):
-  # Filters lst and returns the matching indices
-  matches = []
-  for index in range(len(lst)-1, -1, -1):
-    if pred(lst[index]):
-      matches.append(index)
-  return matches
-
-# mimic matlab's isreal()
-def isreal(arr):
-  # if input is not a list
-  return np.isreal(arr)
-  # if inp is a list:
-  # return all(it == True for it in np.isreal(2))
+from utils.helpers import logger, filter, isreal
 
 class Atmosphere:
   def __init__(self):
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,4 +1,20 @@
 import logging, os
+import numpy as np
 
 logging.basicConfig(level=getattr(logging, os.getenv('DEBUG', 'INFO')))
 logger = logging.getLogger('rocket')
+
+def filter(pred, lst):
+  # Filters a list and returns the matching indices
+  matches = []
+  for index in range(len(lst)-1, -1, -1):
+    if pred(lst[index]):
+      matches.append(index)
+  return matches
+
+# mimic matlab's isreal()
+def isreal(arr):
+  # if input is not a list
+  return np.isreal(arr)
+  # if inp is a list:
+  # return all(it == True for it in np.isreal(2))
