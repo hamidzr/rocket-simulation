5d313b4f7b0d74f0a631e759542f0a537ca7ef4a
Hamid Zare <dellydela@gmail.com>
2018-04-19 15:58:59 -0500

    running condition

--- utils/atmosphere.py
+++ utils/atmosphere.py
@@ -1,17 +1,35 @@
+import numpy as np
+
+def filter(pred, lst):
+  # Filters lst and returns the matching indices
+  matches = []
+  for index in range(len(lst)-1, -1, -1):
+    if pred(lst[index]):
+      matches.append(index)
+  return matches
+
+# mimic matlab's isreal()
+def isreal(arr):
+  # if input is not a list
+  return np.isreal(arr)
+  # if inp is a list:
+  # return all(it == True for it in np.isreal(2))
+
 class Atmosphere:
   def __init__(self):
-    T0_arr = [288.15, 216.65, 216.65, 228.65, 270.65, 270.65, 214.65]
-    p0_arr = [101325, 22632.1, 5474.89, 868.02, 110.91, 66.94, 3.96]
-    B_arr = [-0.0065, 0, 0.001, 0.0028, 0.0, -0.0028, -0.002]
-    z_arr = [0, 11000, 20000, 32000, 47000, 51000, 71000]
-    g0 = 9.81
-    R0 = 6370
-    pMin = 0.000000001
-    Tmin = 3
-    R = 287
+    self.T0_arr = [288.15, 216.65, 216.65, 228.65, 270.65, 270.65, 214.65]
+    self.p0_arr = [101325, 22632.1, 5474.89, 868.02, 110.91, 66.94, 3.96]
+    self.B_arr = [-0.0065, 0, 0.001, 0.0028, 0.0, -0.0028, -0.002]
+    self.z_arr = [0, 11000, 20000, 32000, 47000, 51000, 71000]
+    self.g0 = 9.81
+    self.R0 = 6370
+    self.pMin = 0.000000001
+    self.Tmin = 3
+    self.R = 287
 
   def _Dynamics(self, z):
-    idx = max(find(z >= self.z_arr)) #TODO
+    # idx = max(find(z >= self.z_arr)) #TODO find index of the last item in z_arr that is bigger than is 'le' to z
+    idx = max(filter(lambda x: z >= x, self.z_arr))
     T0 = self.T0_arr[idx]
     p0 = self.p0_arr[idx]
     B = self.B_arr[idx]
@@ -23,7 +41,7 @@ class Atmosphere:
     [T0, p0, B, z0] = self._Dynamics(z)
 
     # Now calculate gravity
-    g = math.pow(self.g0 * (self.R0 / (self.R0 + z/1000)), 2)
+    g = np.power(self.g0 * (self.R0 / (self.R0 + z/1000)), 2)
 
     # Above critical alt?
     if z > 180000:
@@ -40,7 +58,7 @@ class Atmosphere:
       if B == 0:
         p = p0 * exp(-g * (z - z0) / (self.R * T0))
       else:
-        p = p0 * math.pow((T0 / T), (g / (self.R * B)))
+        p = p0 * np.power((T0 / T), (g / (self.R * B)))
       # end
 
       # Keep to a min
--- utils/simulation_data.py
+++ utils/simulation_data.py
@@ -15,84 +15,85 @@ class SimulationData:
     self.x = []
     self.vx = []
     self.ax = []
+
     self.I = 2
 
     self.dt = dt
-    self.t = range(0, tMax, dt) # 0:dt:tMax
-    self.Fd = [0] * len(self.t)
-    self.Ft = [0] * len(self.t)
-    self.m = [0] * len(self.t)
-    self.theta = [0] * len(self.t)
-    self.z = [0] * len(self.t)
-    self.vz = [0] * len(self.t)
-    self.az = [0] * len(self.t)
-    self.x = [0] * len(self.t)
-    self.vx = [0] * len(self.t)
-    self.ax = [0] * len(self.t)
+    self.t = np.arange(0, tMax, dt) # 0:dt:tMax
+    self.Fd = np.zeros(len(self.t))
+    self.Ft = np.zeros(len(self.t))
+    self.m = np.zeros(len(self.t))
+    self.theta = np.zeros(len(self.t))
+    self.z = np.zeros(len(self.t))
+    self.vz = np.zeros(len(self.t))
+    self.az = np.zeros(len(self.t))
+    self.x = np.zeros(len(self.t))
+    self.vx = np.zeros(len(self.t))
+    self.ax = np.zeros(len(self.t))
 
-    def SetInitialConditions(self, mFull):
-      self.z[1] = 0
-      self.vz[1] = 0
-      self.az[1] = -9.81
-      self.x[1] = 0
-      self.vx[1] = 0
-      self.ax[1] = 0
-      self.m[1] = mFull; # Start full
-      self.Fd[1] = 0
-      self.Ft[1] = 0
+  def SetInitialConditions(self, mFull):
+    self.z[1] = 0
+    self.vz[1] = 0
+    self.az[1] = -9.81
+    self.x[1] = 0
+    self.vx[1] = 0
+    self.ax[1] = 0
+    self.m[1] = mFull; # Start full
+    self.Fd[1] = 0
+    self.Ft[1] = 0
 
-    def ResetFrame(self):
-      self.I = 2
+  def ResetFrame(self):
+    self.I = 2
 
-    def Thrust(self, mdot, vjet, Pe, Pa, Ae, N):
-      self.m[self.I] = self.m[self.I-1] - mdot * self.dt * N
-      self.Ft[self.I] = (mdot * vjet + (Pe - Pa) * Ae) * N
-
-
-    def NoThrust(self):
-      self.m[self.I] = self.m[self.I-1]
-      self.Ft[self.I] = 0
+  def Thrust(self, mdot, vjet, Pe, Pa, Ae, N):
+    self.m[self.I] = self.m[self.I-1] - mdot * self.dt * N
+    self.Ft[self.I] = (mdot * vjet + (Pe - Pa) * Ae) * N
 
 
-    def RelativeWind(self):
-      zpart = abs(self.vz[self.I-1])
-      xpart = abs(self.vx[self.I-1])
-      relWind = abs(cos(self.theta[self.I-1]) * zpart) + abs(sin(self.theta[self.I-1]) * xpart)
-      return relWind
-
-    def UpdateVZ(self, dv_axial, g):
-      self.vz[self.I] = self.vz[self.I-1] + dv_axial * cos(self.theta[self.I-1]) - g*self.dt
+  def NoThrust(self):
+    self.m[self.I] = self.m[self.I-1]
+    self.Ft[self.I] = 0
 
 
-    def UpdateAngle(self, dv_axial, thetaWindow, thetaAngle):
-      # Migrate to the angle over first few seconds
-      if self.t[self.I] < thetaWindow:
-        self.theta[self.I] = thetaAngle * (self.t[self.I] / thetaWindow)
-        self.vx[self.I] = tan(self.theta[self.I-1]) * self.vz[self.I-1]
-      else:
-        self.vx[self.I] = self.vx[self.I-1] + dv_axial * sin(self.theta[self.I-1])
-        self.theta[self.I] = atan(self.vx[self.I]/self.vz[self.I])
+  def RelativeWind(self):
+    zpart = abs(self.vz[self.I-1])
+    xpart = abs(self.vx[self.I-1])
+    relWind = abs(np.cos(self.theta[self.I-1]) * zpart) + abs(np.sin(self.theta[self.I-1]) * xpart)
+    return relWind
 
-    def CalculateNewXandZ(self):
-      # Net acceleration
-      dvx = self.vx[self.I] - self.vx[self.I-1]
-      self.ax[self.I] = dvx / self.dt
-      self.x[self.I] = self.x[self.I-1] + 0.5 * dvx * self.dt + self.vx[self.I-1] * self.dt
-      dvz = self.vz[self.I] - self.vz[self.I-1]
-      self.az[self.I] = dvz / self.dt
-      self.z[self.I] = self.z[self.I-1] + 0.5 * dvz * self.dt + self.vz[self.I-1] * self.dt
+  def UpdateVZ(self, dv_axial, g):
+    self.vz[self.I] = self.vz[self.I-1] + dv_axial * np.cos(self.theta[self.I-1]) - g*self.dt
 
-    def NextFrame(self):
-      self.I = self.I + 1
 
-    def RemainingMass(self, m0): #TODO
-      mass = self.m[self.I] - m0
-      return mass
+  def UpdateAngle(self, dv_axial, thetaWindow, thetaAngle):
+    # Migrate to the angle over first few seconds
+    if self.t[self.I] < thetaWindow:
+      self.theta[self.I] = thetaAngle * (self.t[self.I] / thetaWindow)
+      self.vx[self.I] = np.tan(self.theta[self.I-1]) * self.vz[self.I-1]
+    else:
+      self.vx[self.I] = self.vx[self.I-1] + dv_axial * np.sin(self.theta[self.I-1])
+      self.theta[self.I] = np.arctan(self.vx[self.I]/self.vz[self.I]) # WARN atan
 
-    def velocity(self, offset):
-      # v = (self.vz[self.I + offset].^2 + self.vx[self.I + offset].^2).^0.5
-      t1 = math.pow(self.vz[self.I + offset], 2) # WARN changes
-      t2 = math.pow(self.vx[self.I + offset], 2)
-      v = t1 + math.sqrt(t2)
-      return v
+  def CalculateNewXandZ(self):
+    # Net acceleration
+    dvx = self.vx[self.I] - self.vx[self.I-1]
+    self.ax[self.I] = dvx / self.dt
+    self.x[self.I] = self.x[self.I-1] + 0.5 * dvx * self.dt + self.vx[self.I-1] * self.dt
+    dvz = self.vz[self.I] - self.vz[self.I-1]
+    self.az[self.I] = dvz / self.dt
+    self.z[self.I] = self.z[self.I-1] + 0.5 * dvz * self.dt + self.vz[self.I-1] * self.dt
+
+  def NextFrame(self):
+    self.I = self.I + 1
+
+  def RemainingMass(self, m0): #TODO
+    mass = self.m[self.I] - m0
+    return mass
+
+  def velocity(self, offset):
+    # v = (self.vz[self.I + offset].^2 + self.vx[self.I + offset].^2).^0.5
+    t1 = math.pow(self.vz[self.I + offset], 2) # WARN changes
+    t2 = math.pow(self.vx[self.I + offset], 2)
+    v = t1 + math.sqrt(t2)
+    return v
 
