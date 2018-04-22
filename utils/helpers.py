6a4e1ac8ed1e9c9e3d41c6a46824c7c660eb3806
Hamid Zare <dellydela@gmail.com>
2018-04-21 19:14:35 -0500

    added memize decorator

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,4 +1,5 @@
 import logging, os
+from functools import wraps
 import numpy as np
 
 logging.basicConfig(level=getattr(logging, os.getenv('DEBUG', 'INFO')))
@@ -13,8 +14,23 @@ def filter(pred, lst):
   return matches
 
 # mimic matlab's isreal()
-def isreal(arr):
+def isreal(inp):
   # if input is not a list
-  return np.isreal(arr)
+  res = np.isreal(inp)
+  return res
   # if inp is a list:
   # return all(it == True for it in np.isreal(2))
+
+# memoize helper
+def memoize(f):
+    cache = {}
+    @wraps(f)
+    def decorated(*args):
+        key = (f, str(args))
+        result = cache.get(key, None)
+        if result is None:
+            result = f(*args)
+            cache[key] = result
+        return result
+    return decorated
+
